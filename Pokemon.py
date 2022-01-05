9d801b8734e290f2fcc5ec8d1ed0ab0ab06f8cd8
LeeadJ <leead123@gmail.com>
2022-01-05 19:19:47 +0200

    Fixed Pokemon

--- Pokemon.py
+++ Pokemon.py
@@ -6,6 +6,8 @@ class Pokemon:
         self.value = float(data['value'])
         self.type = int(data['type'])
         location = str(data['pos']).split(',')
-        self.pos = {float(location[0]), float(location[1]), float(location[2])}
+        self.pos = [float(location[0]), float(location[1]), float(location[2])]
         self.marked = False
 
+    def __repr__(self):
+        return 'Value: %s Type: %s Pos: %s Marked: %s ' % (self.value, self.type, self.pos, self.marked)
