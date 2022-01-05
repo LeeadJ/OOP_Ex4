476c461b1cdfc853ed565318268ce5027228f5fd
LeeadJ <leead123@gmail.com>
2022-01-05 19:19:36 +0200

    Fixed Agent

--- Agent.py
+++ Agent.py
@@ -9,6 +9,11 @@ class Agent:
         self.dest = int(data['dest'])
         self.speed = float(data['speed'])
         location = str(data['pos']).split(',')
-        self.pos = {float(location[0]), float(location[1]), float(location[2])}
+        self.pos = (float(location[0]), float(location[1]), float(location[2]))
         self.pos = []
         self.pokemon_list = []
+
+    def __repr__(self):
+        return 'ID: %s Value: %s Src: %s Dest: %s ' \
+               'Speed: %s Pos: %s ' \
+               'Pokemons: %s' % (self.id, self.value, self.src, self.dest, self.speed, self.pos, self.pokemon_list)
