27283a7eee6b5cd507a5850c71df0e3ea63c8866
LeeadJ <leead123@gmail.com>
2022-01-08 19:59:01 +0200

    added buttons

--- GUI.py
+++ GUI.py
@@ -2,6 +2,8 @@ import json
 
 import pygame
 from pygame import *
+
+import GameFunc
 from client import Client
 from GameFunc import Game
 
@@ -39,14 +41,15 @@ class GUI:
             self.min_y = min(self.min_y, y)
             self.max_x = max(self.max_x, x)
             self.max_y = max(self.max_y, y)
-        #//////////////
+        # //////////////
         buttonColor = (28, 172, 74)
         buttonWidth = 100
-        self.time_button = button(buttonColor, 1, 2, buttonWidth, 20, 'TIME')
-        self.move_button = button(buttonColor, 101, 2, buttonWidth, 20, 'MOVES')
-        self.grade_button = button(buttonColor, 201, 2, buttonWidth, 20, 'GRADE')
+        self.level_button = button(buttonColor, 1, 2, buttonWidth, 20, 'LEVEL')
+        self.agent_button = button(buttonColor, 101, 2, buttonWidth, 20, 'AGENTS')
+        self.time_button = button(buttonColor, 201, 2, buttonWidth, 20, 'TIME')
+        self.move_button = button(buttonColor, 301, 2, buttonWidth, 20, 'MOVES')
+        self.grade_button = button(buttonColor, 401, 2, buttonWidth, 20, 'GRADE')
         self.stop_button = button(buttonColor, 930, 640, buttonWidth, 20, 'Click to STOP')
-
 
     def scale(self, data, min_screen, max_screen, min_data, max_data):
         return ((data - min_data) / (max_data - min_data)) * (max_screen - min_screen) + min_screen
@@ -110,6 +113,10 @@ class GUI:
     def draw_button(self) -> None:
         self.stop_button.draw(self.screen, (0, 0, 0))
         data = json.loads(self.client.get_info())["GameServer"]
+        self.agent_button.text = 'AGENTS: ' + str(data['agents'])
+        self.agent_button.draw(self.screen, (0, 0, 0))
+        self.level_button.text = 'LEVEL: ' + str(data['game_level'])
+        self.level_button.draw(self.screen, (0, 0, 0))
         self.move_button.text = 'MOVES: ' + str(data['moves'])
         self.move_button.draw(self.screen, (0, 0, 0))
         self.time_button.text = 'TIME: ' + str(int(float(self.client.time_to_end()) / 1000))
@@ -117,7 +124,10 @@ class GUI:
         self.grade_button.text = 'GRADE: ' + str(data["grade"])
         self.grade_button.draw(self.screen, (0, 0, 0))
 
-# Found code from open source (stackoverflow) https://stackoverflow.com/questions/63435298/how-to-create-a-button-class-in-pygame
+
+
+# Found code from open source (stackoverflow)
+# https://stackoverflow.com/questions/63435298/how-to-create-a-button-class-in-pygame
 class button():
     def __init__(self, color, x, y, width, height, text=''):
         self.color = color
@@ -138,12 +148,12 @@ class button():
             font = pygame.font.SysFont('comicsans', 10)
             text = font.render(self.text, 1, (0, 0, 0))
             win.blit(text, (
-            self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+                self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
 
     def isOver(self, pos):
         # Pos is the mouse position or a tuple of (x,y) coordinates
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
+        if self.x < pos[0] < self.x + self.width:
+            if self.y < pos[1] < self.y + self.height:
                 return True
 
         return False
